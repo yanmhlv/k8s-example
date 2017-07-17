72f473b10659ec3509016f81cc9956529804c2c7
Yan Mihailov <yanmhlv@gmail.com>
2017-07-17 23:58:50 +0300

    added new example application with vendor + deployment + service files

---
+++ src/service.2/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+	"io/ioutil"
+)
+
+func main() {
+	n := rand.Intn(100)
+
+	router := httprouter.New()
+	router.GET("/", func(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
+		fmt.Fprintf(w, "service.2 %d", n)
+	})
+
+	router.GET("/:name", func(w http.ResponseWriter, req *http.Request, p httprouter.Params) {
+		urlStr := fmt.Sprintf("http://%s:3000/", p.ByName("name"))
+		resp, err := http.Get(urlStr)
+		if err != nil {
+			fmt.Fprintf(w, "get error: %+v", err)
+			return
+		}
+		defer resp.Body.Close()
+
+		data, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Fprintf(w, "read resp error: %+v", err)
+			return
+		}
+
+		fmt.Fprintf(w, "service-%d: response from %s is [%s]", n, p.ByName("name"), string(data))
+	})
+
+	(&http.Server{
+		Handler: router,
+		Addr:    ":3000",
+	}).ListenAndServe()
+}
